ef1d0340ee3f317d18f512c8d9d74d5930cf5a45
AiAnswer3213 <aigibanswers@outlook.com>
2024-10-21 03:30:32 +0000

    RaidInfo dummy test data

--- RaidInfoFix.lua
+++ RaidInfoFix.lua
@@ -6,6 +6,8 @@
 -- We move the contents of the RaidInfoScrolLFrame to the right by 2 so the ..InstanceName starts at xoffset 0
 -- Move the RaidInfoScrolLFrame to the left by 2 to align it back to the design
 
+-- TODO: The ID header alignment in relation to the ID data alignment looks odd when scrolling is enabled
+
 -- Coordinates are in format: defaultPosition+moveBy
 local function BPHook_RaidInfoFrame_Update()
     for i = 1, MAX_RAID_INFOS do
@@ -27,7 +29,6 @@ local function BPHook_RaidInfoFrame_Update()
         end
     end
 
-    -- TODO: Check the scrollbar is correct when enough saves to test
     local scrollFrame = _G["RaidInfoScrollFrame"]
     if (scrollFrame) then
         local point, relativeTo, relativePoint, xOfs, yOfs = scrollFrame:GetPoint()
@@ -37,3 +38,47 @@ local function BPHook_RaidInfoFrame_Update()
 end
 
 BPHookMaster:DeferredHookSecureHook("BlizzPlease", "RaidInfoFrame_Update", BPHook_RaidInfoFrame_Update)
+
+local ENABLE_TESTS = false
+if ENABLE_TESTS then
+    function GetNumSavedInstances()
+        return 20
+    end
+
+    -- Return RANDOM test data row
+    -- "Coilfang: Serpentshine Cavern", 235, 265123
+    -- "Magtheridon's Lair", 9123, 123456
+    function GetSavedInstanceInfo(index)
+        if (index == 1) then
+            return "TestData: Not a real instance", 123, 456
+        end
+
+        local instances = {
+            { "Coilfang: Serpentshine Cavern"},
+            { "Magtheridon's Lair"},
+            { "Blackwing Lair"},
+            { "Molten Core"},
+            { "Onyxia's Lair"},
+            { "Ruins of Ahn'Qiraj"},
+            { "Temple of Ahn'Qiraj"},
+            { "Zul'Gurub"},
+            { "Zul'Aman"},
+            { "Karazhan"},
+            { "Gruul's Lair"},
+            { "Magtheridon's Lair"},
+            { "Serpentshrine Cavern"},
+            { "The Eye"},
+            { "Battle for Mount Hyjal"},
+            { "Black Temple"},
+            { "Sunwell Plateau"},
+            { "Naxxramas"},
+            { "The Obsidian Sanctum"},
+            { "The Eye of Eternity"}
+        }
+
+        local randomIndex = math.random(1, #instances)
+        local randomID = math.random(1, 9999)
+        local randomReset = math.random(1, 999999)
+        return instances[randomIndex][1], randomID, randomReset
+    end
+end
